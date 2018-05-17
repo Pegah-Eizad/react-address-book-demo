858ce6380f3f36157bb803e80ccb27f0d6c46be2
Pegah Eizadkhah <peizadkh@gmail.com>
2018-05-17 15:29:25 -0400

    implemented react js structure

--- src/components/address-book.js
+++ src/components/address-book.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
 import Contact from './contact';
-
 // The AddressBook component goes here.  It should be the default export.
+export default class AddressBook extends React.component{
+  constructor(props){
+    super(props);
+    this.state = {
+        contacts: [{
+            name: 'Alice Aardvark',
+            photo: 'https://api.adorable.io/avatars/64/alice%40thinkful.com',
+            address: '1600 Pennsylvania Ave'
+        }, {
+            name: 'Bob Bear',
+            photo: 'https://api.adorable.io/avatars/64/bob%40thinkful.com',
+            address: '725 5th Ave'
+        }, {
+            name: 'Carol Coyote',
+            photo: 'https://api.adorable.io/avatars/64/carol%40thinkful.com',
+            address: '4 Pennsylvania Plaza'
+        }]
+    }
+  }
+
+  render(){
+    return();
+  }
+}
--- src/components/contact.js
+++ src/components/contact.js
@@ -3,3 +3,18 @@ import React from 'react';
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+export default function Contact(props){
+  return(
+    <div className="contact">
+        <img className="contact-photo">{props.photo} />
+        <h1 className="contact-name">{props.name}</h1>
+        <h1 className="contact-address">{props.address}</h1>
+    </div
+  );
+};
+
+Contact.defaultProps = {
+  name: '';
+  photo: '',
+  address: ''
+};
